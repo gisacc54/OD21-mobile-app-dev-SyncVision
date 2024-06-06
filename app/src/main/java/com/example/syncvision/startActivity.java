05291081b5ad9441dd41d1df87b8c5325f1f8e1a
DARKLORD <harlembenjy1@gmail.com>
2024-06-06 08:50:44 +0300

    new

--- app/src/main/java/com/example/syncvision/startActivity.java
+++ app/src/main/java/com/example/syncvision/startActivity.java
@@ -1,14 +1,28 @@
 package com.example.syncvision;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Objects;
+
 public class startActivity extends AppCompatActivity {
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch switch1;
+    boolean nightMODE;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,31 @@ public class startActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        switch1 = findViewById(R.id.switch1);
+        sharedPreferences = getSharedPreferences( "MODE", Context.MODE_PRIVATE);
+        nightMODE = sharedPreferences.getBoolean("night", false );
+
+
+        if (nightMODE){
+            switch1.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        switch1.setOnClickListener(v -> {
+
+            if (nightMODE){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                editor = sharedPreferences.edit();
+                editor.putBoolean("night", false);
+            } else
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                editor = sharedPreferences.edit();
+                editor.putBoolean("night", true);
+            }
         });
     }
 }
