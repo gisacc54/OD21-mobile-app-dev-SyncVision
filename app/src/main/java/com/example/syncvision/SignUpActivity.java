6b1e2763a04529748c654282e9b97e8d2bd9068a
gisacc54 <giftisacc54@gmail.com>
2024-07-01 23:22:58 +0300

    update

--- app/src/main/java/com/example/syncvision/SignUpActivity.java
+++ app/src/main/java/com/example/syncvision/SignUpActivity.java
@@ -4,13 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText fullNameET;
@@ -20,26 +17,29 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
         Button button = findViewById(R.id.sign_up_btn);
-        fullNameET = findViewById(R.id.sign_up_full_name);
-        emailET = findViewById(R.id.sign_up_email);
-        passwordET = findViewById(R.id.sign_up_password);
+        TextView textView = findViewById(R.id.or_login);
+
+         fullNameET = findViewById(R.id.sign_up_full_name);
+         emailET = findViewById(R.id.sign_up_email);
+         passwordET = findViewById(R.id.sign_up_password);
 
         button.setOnClickListener(this);
+        textView.setOnClickListener(this);
+    }
+
+    private void signUp(){
+        String fullName = fullNameET.getText().toString();
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+        Toast.makeText(SignUpActivity.this, "Full Name: "+fullName, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onClick(View v) {
-        if ( v.getId() == R.id.sign_up_btn){
-            this.signUp();
+        if (v.getId() == R.id.sign_up_btn){
+            signUp();
         }
-    }
 
-    private   void signUp(){
-        String fullName = fullNameET.getText().toString();
-        String email = emailET.getText().toString();
-        String password = passwordET.getText().toString();
-        Toast.makeText(SignUpActivity.this, "Full Name:"+fullName, Toast.LENGTH_SHORT).show();
     }
 }
